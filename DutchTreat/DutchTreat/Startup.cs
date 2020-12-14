a7ca4bc5f073a7ff9ec3589e557f8264a6e6b073
chandrakanth suresh <chandru.chawan@gmail.com>
2020-12-14 17:23:04 -0500

    fundamentals

--- DutchTreat/DutchTreat/Startup.cs
+++ DutchTreat/DutchTreat/Startup.cs
@@ -21,16 +21,9 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseNodeModules();
         }
     }
 }
